416df287ea3e5eeb550a60a00c679d4e97d17862
Stefan <stefan.jovanavich@gmail.com>
2020-07-22 09:11:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/passwordstrengthcalculator/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.passwordstrengthcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var color: Int = R.color.weak
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val passwordStrengthCalculator = PasswordStrengthCalculator()
+        password_input.addTextChangedListener(passwordStrengthCalculator)
+
+        // Observers
+        passwordStrengthCalculator.strengthLevel.observe(this, Observer {strengthLevel ->
+            displayStrengthLevel(strengthLevel)
+        })
+        passwordStrengthCalculator.strengthColor.observe(this, Observer {strengthColor ->
+            color = strengthColor
+        })
+
+        passwordStrengthCalculator.lowerCase.observe(this, Observer {value ->
+            displayPasswordSuggestions(value, lowerCase_img, lowerCase_txt)
+        })
+        passwordStrengthCalculator.upperCase.observe(this, Observer {value ->
+            displayPasswordSuggestions(value, upperCase_img, upperCase_txt)
+        })
+        passwordStrengthCalculator.digit.observe(this, Observer {value ->
+            displayPasswordSuggestions(value, digit_img, digit_txt)
+        })
+        passwordStrengthCalculator.specialChar.observe(this, Observer {value ->
+            displayPasswordSuggestions(value, specialChar_img, specialChar_txt)
+        })
+
+    }
+
+    private fun displayPasswordSuggestions(value: Int, imageView: ImageView, textView: TextView) {
+        if(value == 1){
+            imageView.setColorFilter(ContextCompat.getColor(this, R.color.bulletproof))
+            textView.setTextColor(ContextCompat.getColor(this, R.color.bulletproof))
+        }else{
+            imageView.setColorFilter(ContextCompat.getColor(this, R.color.darkGray))
+            textView.setTextColor(ContextCompat.getColor(this, R.color.darkGray))
+        }
+    }
+
+    private fun displayStrengthLevel(strengthLevel: String) {
+        button.isEnabled = strengthLevel.contains("BULLETPROOF")
+
+        strength_level_txt.text = strengthLevel
+        strength_level_txt.setTextColor(ContextCompat.getColor(this, color))
+        strength_level_indicator.setBackgroundColor(ContextCompat.getColor(this, color))
+    }
+
+}
---
+++ app/src/main/java/com/example/passwordstrengthcalculator/PasswordStrengthCalculator.kt
@@ -0,0 +1,79 @@
+package com.example.passwordstrengthcalculator
+
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.lifecycle.MutableLiveData
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class PasswordStrengthCalculator: TextWatcher {
+
+    var strengthLevel: MutableLiveData<String> = MutableLiveData()
+    var strengthColor: MutableLiveData<Int> = MutableLiveData()
+
+    var lowerCase: MutableLiveData<Int> = MutableLiveData(0)
+    var upperCase: MutableLiveData<Int> = MutableLiveData(0)
+    var digit: MutableLiveData<Int> = MutableLiveData(0)
+    var specialChar: MutableLiveData<Int> = MutableLiveData(0)
+
+    override fun afterTextChanged(p0: Editable?) {}
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    override fun onTextChanged(char: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        if(char != null){
+            lowerCase.value = if (char.hasLowerCase()) { 1 } else { 0 }
+            upperCase.value = if (char.hasUpperCase()) { 1 } else { 0 }
+            digit.value = if (char.hasDigit()) { 1 } else { 0 }
+            specialChar.value = if (char.hasSpecialChar()) { 1 } else { 0 }
+            calculateStrength(char)
+        }
+    }
+
+    private fun calculateStrength(password: CharSequence) {
+        if(password.length in 0..7){
+            strengthColor.value = R.color.weak
+            strengthLevel.value = "WEAK"
+        }else if(password.length in 8..10){
+            if(lowerCase.value == 1 || upperCase.value == 1 || digit.value == 1 || specialChar.value == 1){
+                strengthColor.value = R.color.medium
+                strengthLevel.value = "MEDIUM"
+            }
+        }else if(password.length in 11..16){
+            if(lowerCase.value == 1 || upperCase.value == 1 || digit.value == 1 || specialChar.value == 1){
+                if(lowerCase.value == 1 && upperCase.value == 1){
+                    strengthColor.value = R.color.strong
+                    strengthLevel.value = "STRONG"
+                }
+            }
+        }else if(password.length > 16){
+            if(lowerCase.value == 1 && upperCase.value == 1 && digit.value == 1 && specialChar.value == 1){
+                strengthColor.value = R.color.bulletproof
+                strengthLevel.value = "BULLETPROOF"
+            }
+        }
+    }
+
+    private fun CharSequence.hasLowerCase(): Boolean{
+        val pattern: Pattern = Pattern.compile("[a-z]")
+        val hasLowerCase: Matcher = pattern.matcher(this)
+        return hasLowerCase.find()
+    }
+
+    private fun CharSequence.hasUpperCase(): Boolean{
+        val pattern: Pattern = Pattern.compile("[A-Z]")
+        val hasUpperCase: Matcher = pattern.matcher(this)
+        return hasUpperCase.find()
+    }
+
+    private fun CharSequence.hasDigit(): Boolean{
+        val pattern: Pattern = Pattern.compile("[0-9]")
+        val hasDigit: Matcher = pattern.matcher(this)
+        return hasDigit.find()
+    }
+
+    private fun CharSequence.hasSpecialChar(): Boolean{
+        val pattern: Pattern = Pattern.compile("[!@#$%^&*()_=+{}/.<>|\\[\\]~-]")
+        val hasSpecialChar: Matcher = pattern.matcher(this)
+        return hasSpecialChar.find()
+    }
+
+}
