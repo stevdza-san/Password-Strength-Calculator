de6427875361c1997249e6bf69e3c3f34f4f29cd
Stefan <stefan.jovanavich@gmail.com>
2020-07-22 09:32:28 +0200

    Added new enum class.

---
+++ app/src/main/java/com/example/passwordstrengthcalculator/StrengthLevel.kt
@@ -0,0 +1,8 @@
+package com.example.passwordstrengthcalculator
+
+enum class StrengthLevel(val level: String) {
+    WEAK("WEAK"),
+    MEDIUM("MEDIUM"),
+    STRONG("STRONG"),
+    BULLETPROOF("BULLETPROOF")
+}
